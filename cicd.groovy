cf1cf87aabc370688fb1a4643e055e7be5e5db7b
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:47:06 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'tclport'), string(name: 'DESCRIPTION', 'tclport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'tclport'), string(name: 'DESCRIPTION', 'Tcl is a high-level, general-purpose, interpreted, dynamic programming language.' )]
         }
 }

3035c6f4cf2df6373f0aaec266b4f4153e9a4564
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 09:41:44 -0400

    Add polling for changes in the jenkins script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,23 @@
 node('linux') 
 {
+        stage ('Poll') {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/tclport.git']]])
+                ])
+
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/utils.git']]])
+                ])
+        }
+
         stage('Build') {
                 build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'tclport'), string(name: 'DESCRIPTION', 'Tcl is a high-level, general-purpose, interpreted, dynamic programming language.' )]
         }

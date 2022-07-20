191c63589c7bd8efbbd54e8ca8c2f7b913faa7c9
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-20 09:55:14 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'tclport'), string(name: 'DESCRIPTION', value: 'Tcl is a high-level, general-purpose, interpreted, dynamic programming language.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'tclport'), string(name: 'PORT_DESCRIPTION', value: 'Tcl is a high-level, general-purpose, interpreted, dynamic programming language.' )]
         }
 }

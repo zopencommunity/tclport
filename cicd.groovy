71e61a5ffdceeec8d24c2f899f0b6d34c5e2692e
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:38 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/tclport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/tclport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/tclport.git'), string(name: 'PORT_DESCRIPTION', value: 'Tcl is a high-level, general-purpose, interpreted, dynamic programming language.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/tclport.git'), string(name: 'PORT_DESCRIPTION', value: 'Tcl is a high-level, general-purpose, interpreted, dynamic programming language.' )]
         }
 }
